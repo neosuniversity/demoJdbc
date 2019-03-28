11f60fad07dd5a78c7e2eaae95fd494bc265412c
Mario <mario.hidalgom@yahoo.com.mx>
2019-03-27 21:13:07 -0600

    samples

--- src/com/neosuniversity/ui/DemoInsertSQL.java
+++ src/com/neosuniversity/ui/DemoInsertSQL.java
@@ -1,8 +1,36 @@
 package com.neosuniversity.ui;
 
+import com.neosuniversity.data.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DemoInsertSQL {
 
     public static void main(String[] args) {
+
+        Connection con = ConnectionFactory.getConnection();
+
+        try {
+            PreparedStatement stm = con.prepareStatement("insert into proveedor  " +
+                    "values ( ? ,? ,?, ? ,?)");
+
+
+            stm.setInt(1,  5);
+            stm.setString(2,"Samsung");
+            stm.setString(3, "Mi direccion");
+            stm.setString(4, "45-5-4");
+            stm.setString(5, "prov@mimail.com");
+
+           int res = stm.executeUpdate();
+
+            System.out.println("Regs insertados " + res);
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
